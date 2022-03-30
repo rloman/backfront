081fc9b4d1e373eb593ebdcbfcb710a0fa3e823b
David Nieuwenhuizen <david@laudav.nl>
2022-03-30 16:38:58 +0200

    link met online backend

--- script/users.js
+++ script/users.js
@@ -17,7 +17,7 @@ async function run() {
     const users = await fetchUsers();
 
     for (const user of users) {
-        const endPoint = await fetch("http://localhost:8082/new", {
+        const endPoint = await fetch("https://bordspelbackend.azurewebsites.net/new", {
             method: "POST",
             headers: {
                 "Accept": "application/json",
@@ -36,44 +36,41 @@ async function run() {
     let currentUser = localStorage.getItem("user");
     if (!currentUser) {
         localStorage.setItem("user", JSON.stringify(users[0]));
-        const saveId = async () => {
-            const id = await logIn(users[0]);
+        logIn(users[0], (id) => {
             localStorage.setItem("ID",id);
-            console.log(id);
-        }
-        saveId();
-        window.location.reload();
-        return
-    }
 
-    currentUser = JSON.parse(currentUser);
-
-    setUserAvatar(currentUser);
-    let temp = `<select id="userSelect">`;
-    users.forEach((user) => {
-        let selected = false;
-        if (currentUser.firstName === user.firstName) {
-            selected = true;
-        }
-        temp += `
-        <option ${selected ? "selected" : ""} value="${user.firstName}">${user.firstName}</option>
-      `;
-    });
-    temp += `</select>`;
-    document.body.innerHTML += temp;
-    const userSelect = document.getElementById("userSelect");
-    userSelect.addEventListener("change", (ev) => {
-        const selectedUser = users.find((user) => {
-            return user.firstName === ev.target.value
+            window.location.reload();
         });
-        localStorage.setItem("user", JSON.stringify(selectedUser));
-        const saveId = async () => {
-            const id = await logIn(selectedUser);
-            localStorage.setItem("ID",id);
-        }
-        saveId();
-        setUserAvatar(selectedUser);
-    });
+    } else {
+        currentUser = JSON.parse(currentUser);
+
+        setUserAvatar(currentUser);
+        let temp = `<select id="userSelect">`;
+        users.forEach((user) => {
+            let selected = false;
+            if (currentUser.firstName === user.firstName) {
+                selected = true;
+            }
+            temp += `
+            <option ${selected ? "selected" : ""} value="${user.firstName}">${user.firstName}</option>
+          `;
+        });
+        temp += `</select>`;
+        document.body.innerHTML += temp;
+        const userSelect = document.getElementById("userSelect");
+        userSelect.addEventListener("change", (ev) => {
+            const selectedUser = users.find((user) => {
+                return user.firstName === ev.target.value
+            });
+            localStorage.setItem("user", JSON.stringify(selectedUser));
+            logIn(selectedUser, (id) => {
+                localStorage.setItem("ID",id);
+    
+                window.location.reload();
+            });
+            setUserAvatar(selectedUser);
+        });
+    }
 }
 run();
 
@@ -81,15 +78,14 @@ function setUserAvatar(user) {
     document.getElementById("userAvatar").setAttribute("src", user.avatar)
 }
 
-async function logIn(user) {
-
-    const endPoint = await fetch("http://localhost:8082/login/"+user.email, {
+function logIn(user, afterLogin) {
+    fetch("https://bordspelbackend.azurewebsites.net/login/"+user.email, {
         method: "GET",
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json"
         },
-    });
-    const content = await endPoint.json();
-    return content;
+    })
+    .then(response => response.json())
+    .then(data => afterLogin(data));
 }
