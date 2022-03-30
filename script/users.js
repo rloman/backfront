26405777a1470c04f53ccc1ca586b5bd12b3c030
David Nieuwenhuizen <david@laudav.nl>
2022-03-30 14:12:29 +0200

    User switch connection met backEnd

--- script/users.js
+++ script/users.js
@@ -15,9 +15,33 @@ async function fetchUsers() {
 
 async function run() {
     const users = await fetchUsers();
+
+    for (const user of users) {
+        const endPoint = await fetch("http://localhost:8082/new", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                gebruikersNaam: user.email,
+                wachtwoord: user.password,
+                displayNaam: user.firstName,
+                beschrijving: "",
+                profilePicture: user.avatar
+            })
+        });
+    };
+
     let currentUser = localStorage.getItem("user");
     if (!currentUser) {
         localStorage.setItem("user", JSON.stringify(users[0]));
+        const saveId = async () => {
+            const id = await logIn(users[0]);
+            localStorage.setItem("ID",id);
+            console.log(id);
+        }
+        saveId();
         window.location.reload();
         return
     }
@@ -43,6 +67,11 @@ async function run() {
             return user.firstName === ev.target.value
         });
         localStorage.setItem("user", JSON.stringify(selectedUser));
+        const saveId = async () => {
+            const id = await logIn(selectedUser);
+            localStorage.setItem("ID",id);
+        }
+        saveId();
         setUserAvatar(selectedUser);
         setUserData(selectedUser);
     });
@@ -92,4 +121,17 @@ function setUserData(user) {
                 ${user.email}
             </p>
         `;
+}
+
+async function logIn(user) {
+
+    const endPoint = await fetch("http://localhost:8082/login/"+user.email, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+    });
+    const content = await endPoint.json();
+    return content;
 }
